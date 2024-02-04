1af97ab93aa9090d8a29d9c44155f51d9afe92d5
albabernal03 <ALBABERO@MYUAX.COM>
2024-02-04 21:43:11 +0100

    una vez que entendemos el contenido pasamos al analisis

--- analisis.py
+++ analisis.py
@@ -1,7 +1,12 @@
 import sqlite3
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Conectarse a la base de datos SQLite
 conn=sqlite3.connect('bookmaker.db')
 
-#
+#leer la tabla de equipos
+equipos=pd.read_sql_query("SELECT * FROM equipos",conn)
+
+#Empezamos con el análisis de los datos
